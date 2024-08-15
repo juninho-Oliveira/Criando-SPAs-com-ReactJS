325f2332b64ec41384f617a4edc3d3974b4a3d4a
junior <junior.oliveira05@icloud.com>
2024-08-15 09:16:21 -0300

    estrutura da aplicação - cores & fonte

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -17,9 +17,15 @@ export const ButtonContainer = styled.button<ButtonContainerProps> `
 
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
-        return css`background-color: ${ButtonVariants[props.variant]}`
-    }}
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme.white};
+
+    /*${props => {
+        return `background-color: ${ButtonVariants[props.variant]}`
+    }}*/
 
 `
