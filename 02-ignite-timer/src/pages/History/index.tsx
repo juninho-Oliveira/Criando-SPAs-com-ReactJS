21352139395f84117ac8e9576ac5d9f900d7a9da
junior <junior.oliveira05@icloud.com>
2024-08-21 08:34:03 -0300

    Páginas e rotas - Página: History

---
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -0,0 +1,43 @@
+import { HistoryContainer, HistoryList } from "./style";
+
+export function History () {
+    return (
+       <HistoryContainer>
+         <h1>Meu histórico</h1>
+
+         <HistoryList>
+            <table>
+              <thead>
+                <tr>
+                  <th>Tarefa</th>
+                  <th>Duração</th>
+                  <th>Inicio</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há dois meses</td>
+                  <td>concluído</td>
+                </tr>
+                <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há dois meses</td>
+                  <td>concluído</td>
+                </tr>
+                <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há dois meses</td>
+                  <td>concluído</td>
+                </tr>
+                
+              </tbody>
+            </table>
+         </HistoryList>
+       </HistoryContainer>
+    )
+}
